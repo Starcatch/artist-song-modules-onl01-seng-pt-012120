86488af4c60421d72c64023ec211159d8dda9e87
Timur Mishiev <timurmishiev@gmail.com>
2020-02-25 21:49:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,24 +3,21 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
   include Paramable
-  
+
   @@artists = []
 
   def initialize
-   super
+    super
     @songs = []
   end
-
 
   def self.all
     @@artists
   end
-
 
   def add_song(song)
     @songs << song
